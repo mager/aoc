3fa42502f24fc760406c30d6808b597b9dae2a04
Mager <andrew.mager@gmail.com>
2022-12-04 10:47:44 -0600

    Clean

--- 2022/02/rust/target/debug/deps/part_one-f7424d727ba3f8c8.d
+++
@@ -1,5 +0,0 @@
-/Users/mager/Code/aoc/2022/02/rust/target/debug/deps/part_one-f7424d727ba3f8c8: src/bin/part_one.rs
-
-/Users/mager/Code/aoc/2022/02/rust/target/debug/deps/part_one-f7424d727ba3f8c8.d: src/bin/part_one.rs
-
-src/bin/part_one.rs:
--- 2022/02/rust/target/debug/part_one.d
+++
@@ -1,1 +0,0 @@
-/Users/mager/Code/aoc/2022/02/rust/target/debug/part_one: /Users/mager/Code/aoc/2022/02/rust/src/bin/part_one.rs
